ea93ab39788e2323332ec6325595189d1c952084
wi2 <contact@wi2prod.fr>
2015-08-13 23:23:57 +0200

    new test

--- test.js
+++ test.js
@@ -3,9 +3,9 @@ var Store = require('./index.js').Store;
 var StoreItem = require('./index.js').StoreItem;
 var StoreCollection = require('./index.js').StoreCollection;
 
-// var sailsIOClient = require('sails.io.js');
-// global.io = sailsIOClient(require('socket.io-client'));
-// global.io.sails.url = 'http://localhost:1337';
+var sailsIOClient = require('sails.io.js');
+global.io = sailsIOClient(require('socket.io-client'));
+global.io.sails.url = 'http://localhost:1337';
 
 describe('Simulate wevsocket events with onChange function', function() {
 
@@ -104,12 +104,69 @@ describe('Simulate wevsocket events with onChange function', function() {
     });
     it('should update name to Jo', function() {
       storeI.update({name: 'Jo'});
+      assert.equal(storeI.value.name, 'Jo');
     });
     it('should emit a updated event and return an object have Name is Johnny', function() {
       storeI.on('update', function(data){
         assert.equal(data.name, 'Johnny');
       });
       storeI.onChange({verb: 'updated', data:{name:"Johnny"}});
+    });
+
+
+  });
+
+  describe('When use StorePostItem', function() {
+    var storeP;
+    before(function(done) {
+      storeP = new StoreItem({
+        identity: "post",
+        value: {id: 5}
+      });
+      done()
+    });
+    it('url should equal data', function(done) {
+      storeP.socket.get(function(err, data){
+        assert.equal(storeP.socket.url, '/post/5');
+        assert.equal(typeof data.body, 'object');
+        done();
+      });
+    });
+    it('url should equal data', function(done) {
+      storeP.socket.put({title: 'top'}, function(err, data){
+        assert.equal(storeP.socket.url, '/post/5');
+        assert.equal(data.body.title, 'top');
+        done();
+      });
+    });
+    it('url should equal data', function(done) {
+      storeP.socket.post({title: 'toptop'}, function(err, data){
+        assert.equal(storeP.socket.url, '/post/5');
+        assert.equal(data.body.title, 'toptop');
+        done();
+      });
+    });
+    it('url should equal data', function(done) {
+      storeP.socket.delete(storeP.value.id, function(err, data){
+        assert.equal(typeof storeP.value.title, 'undefined');
+        done();
+      });
+    });
+  });
+
+  describe('When use StorePostItem', function() {
+    var storePP;
+    before(function(done) {
+      storePP = new StoreCollection({
+        identity: "post"
+      });
+      done()
+    });
+    it('url should equal data', function(done) {
+      storePP.socket.post({title: 'top', content: 'content...'}, function(err, data){
+        assert.equal(data.body.title, 'top');
+        done();
+      });
     });
   });
 
